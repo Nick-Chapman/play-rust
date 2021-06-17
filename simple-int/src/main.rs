48f5c768137068ade374b52b4dd0338494aeab91
Nick Chapman <nic@amadido.co.uk>
2021-06-17 19:50:36 +0100

    trees in arenas

--- simple-int/src/main.rs
+++ simple-int/src/main.rs
@@ -1,10 +1,10 @@
 
 //mod simple_interpreter;
-//mod tree;
-mod arena;
+mod tree;
+//mod arena;
 
 fn main() {
-    arena::main();
-    //tree::main();
+    //arena::main();
+    tree::main();
     //simple_interpreter::main();
 }
--- simple-int/src/tree.rs
+++ simple-int/src/tree.rs
@@ -1,76 +1,70 @@
 
-use std::cell::RefCell;
-use std::cell::Ref;
-//use std::cell::Cell;
+use std::sync::Mutex;
+use std::marker;
 
 pub fn main() {
-    println!("**tree experiments**");
-
-    //let mut ts : Vec<Tree> = Vec::new();
-    let collector = Collector::new();
-
-    let t1 = Tree::Leaf(1);
-    collector.push(t1);
-    //ts.push(t1);
-    //let t1 = &ts[0];
-    let t1 = &t1;
-
-    let t2 = Tree::Leaf(2);
-    collector.push(t2);
-    //ts.push(t2);
-    //let t2 = &ts[1];
-    let t2 = &t2;
-
-    let t3 = Tree::Node(t1,t2);
-    let t3 = &t3;
-
-    println!("tree: {:?} --> {}",t1, sum_tree(t1));
-    println!("tree: {:?} --> {}",t3, sum_tree(t3));
-    println!("tree: {:?} --> {}",t2, sum_tree(t2));
+    println!("**tree take2**");
+    let res = {
+        let a : Arena = Arena::new();
+        let x = a.alloc(Tree::Leaf(11));
+        let y = a.alloc(Tree::Leaf(22));
+        let z = a.alloc(Tree::Node(x,y));
+        let p = a.alloc(Tree::Node(y,z));
+        let q = a.alloc(Tree::Node(p,p));
+        sum_tree(q)
+    };
+    println!["res: {}", res];
 }
-
-struct Collector<'a> {
-    a_tree : Tree<'a>,
-    //rc : RefCell<Vec<Tree<'a>>>,
-    rc : RefCell<Vec<RefCell<Tree<'a>>>>
-}
-
-impl<'a> Collector<'a> {
-    fn new() -> Collector<'a> {
-        let a_tree = Tree::Leaf(99);
-        //let rc = RefCell::new(Vec::new());
-        let rc = RefCell::new(Vec::new());
-        Collector { a_tree, rc }
-    }
-    fn push(&self, tree: Tree<'a>) {
-        self.rc.borrow_mut().push(RefCell::new(tree));
-    }
-    fn ref0(&'a self) -> &'a Tree<'a> {
-        & self.a_tree
-        //let _vec = self.rc.borrow();
-        //& _vec[0]
-        //self.rc.borrow().get(0).unwrap()
-        //panic![""]
-    }
-    
-    fn ref1(&'a self) -> Ref<'a,Tree<'a>> {
-        //let v = self.rc.(); //.borrow();
-        //let c = v[0].borrow();
-        //c
-        panic![""]
-    }
-}
-
 
 #[derive(Debug,Copy,Clone)]
 enum Tree<'a> {
+    Empty,
     Leaf(u32),
     Node(&'a Tree<'a>,&'a Tree<'a>)
 }
 
 fn sum_tree(tree: &Tree) -> u32 {
     match *tree {
+        Tree::Empty => 0,
         Tree::Leaf(n) => n,
         Tree::Node(t1,t2) => sum_tree(t1) + sum_tree(t2)
+    }
+}
+
+#[derive(Debug)]
+struct Arena<'a> {
+    buffer: Mutex<Buffer<'a>>,
+    _marker: marker::PhantomData<&'a Tree<'a>>,
+}
+
+impl<'a> Arena<'a> {
+    fn new() -> Arena<'a> {
+        Arena {
+            buffer: Mutex::new(Buffer::new()),
+            _marker: marker::PhantomData
+        }
+    }
+    fn alloc(&self, thing: Tree<'a>) -> &'a Tree<'a> {
+        let buffer = &mut self.buffer.lock().unwrap();
+        let next = buffer.next;
+        buffer.next += 1;
+        let hold = &mut buffer.array[next];
+        *hold = thing;
+        unsafe { & * (hold as *const Tree) }
+    }
+}
+
+const BUFFER_SIZE : usize = 10;
+
+#[derive(Debug)]
+struct Buffer<'a> {
+    next: usize,
+    array: [Tree<'a>;BUFFER_SIZE],
+}
+
+impl<'a> Buffer<'a> {
+    fn new() -> Buffer<'a> {
+        let def = Tree::Empty;
+        Buffer { next: 0, array: [def;BUFFER_SIZE] }
     }
 }
